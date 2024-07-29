f418224d9c74ea3fc828f6a83228f87be3e119bf
DeviantNinja <73482554+DeviantNinja@users.noreply.github.com>
2024-07-29 19:20:29 +0100

    test

---
+++ src/Libary.java
@@ -0,0 +1,20 @@
+public class Libary {
+
+    public Libary() {
+
+    }
+
+    public void createBook(String title, String author){
+        // todo
+    }
+
+    public void createUser() {
+        //todo
+    }
+    
+    public void displayBooks() {
+        // todo
+    }
+
+
+}
